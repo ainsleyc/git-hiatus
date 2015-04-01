c422ad019b32940fe073679df677d563094f5015
Ainsley Chong <ainsley@factual.com>
2015-04-01 07:28:01 -0700

    Added subfolders

---
+++ hiatus/scheduler.go
@@ -0,0 +1,13 @@
+package hiatus
+
+import (
+	"fmt"
+)
+
+func ReadScheduled() {
+	fmt.Println("ReadScheduled")
+}
+
+func ReadCommited() {
+	fmt.Println("ReadCommited")
+}
---
+++ hiatus/scheduler_test.go
@@ -0,0 +1,13 @@
+package hiatus
+
+import (
+	"testing"
+)
+
+func TestReadScheduled(t *testing.T) {
+	ReadScheduled()
+}
+
+func TestReadCommited(t *testing.T) {
+	ReadCommited()
+}
